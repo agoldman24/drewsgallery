ee92cc3989dad088abcf43add228375f179ee05b
Andrew Goldman <abg@vt.edu>
2022-11-27 10:51:03 -0500

    material-ui dialog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { Fragment, useState } from "react";
+import { Dialog, withStyles } from "@material-ui/core";
 import "./index.css";
 import { images } from "./images/images";
 import Gallery from "react-photo-gallery";
@@ -6,7 +7,16 @@ import Carousel, { Modal, ModalGateway } from "react-images";
 import ZoomImage from "./ZoomImage";
 import GalleryImage from "./GalleryImage";
 
-const App = () => {
+const styles = () => ({
+  root: {
+    position: "initial",
+  },
+  backdropRoot: {
+    background: "none",
+  },
+});
+
+const App = ({ classes }) => {
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -35,49 +45,57 @@ const App = () => {
         style={{ opacity: 0.2, width: "300%" }}
         className="cloud"
       />
-      <div
-        style={{
-          position: "fixed",
-          top: "0",
-          height: "calc(100% - 20px)",
-          width: "calc(100% - 20px)",
-          padding: 10,
-          overflow: "auto",
+      <Dialog
+        open={true}
+        classes={{
+          root: classes.root,
+        }}
+        BackdropProps={{
+          classes: {
+            root: classes.backdropRoot,
+          },
         }}
       >
-        <Gallery
-          photos={images}
-          margin={5}
-          renderImage={(image) => (
-            <GalleryImage
-              image={image}
-              key={image.photo.src}
-              openLightbox={openLightbox}
-            />
-          )}
-        />
-        <ModalGateway>
-          {viewerIsOpen ? (
-            <Modal onClose={closeLightbox}>
-              <Carousel
-                currentIndex={currentImage}
-                views={images.map((x) => ({
-                  ...x,
-                  srcset: x.srcSet,
-                  caption: x.title,
-                  source: x.src,
-                }))}
-                components={{
-                  View: (props) => <ZoomImage {...props} />,
-                }}
-                showNavigationOnTouchDevice={true}
+        <div
+          style={{
+            top: "0",
+            left: "0",
+            width: "calc(100% - 10px)",
+            height: "calc(100% - 10px)",
+            position: "fixed",
+            overflowY: "auto",
+            padding: "5px",
+          }}
+        >
+          <Gallery
+            photos={images}
+            renderImage={(image) => (
+              <GalleryImage
+                image={image}
+                key={image.photo.src}
+                openLightbox={openLightbox}
               />
-            </Modal>
-          ) : null}
-        </ModalGateway>
-      </div>
+            )}
+            margin={5}
+          />
+          <ModalGateway>
+            {viewerIsOpen && (
+              <Modal onClose={closeLightbox}>
+                <Carousel
+                  currentIndex={currentImage}
+                  views={images.map((x) => ({ ...x, source: x.src }))}
+                  components={{
+                    View: (props) => <ZoomImage {...props} />,
+                  }}
+                  showNavigationOnTouchDevice={true}
+                />
+              </Modal>
+            )}
+          </ModalGateway>
+        </div>
+      </Dialog>
     </Fragment>
   );
 };
 
-export default App;
+export default withStyles(styles)(App);
